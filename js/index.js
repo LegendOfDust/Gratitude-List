f5326c63197d98bf0ebe2becaf62ea2776b87e61
Ashlyn Carter <eannacarter@gmail.com>
2024-11-04 10:15:11 -0500

    Commented the JS file

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
-console.log("connected");
-
 const makeList = () => {
-    //get content from input box
+    //GET CONTENT FROM INPUT BOX
     const input = document.getElementById('thing-to-make');
     const thingToMake = input.value;
-    //add it to list
+    
+    //ADDING THE INPUT TO THE ONSCREEN LIST
     const theList = document.getElementById('the-list');
+    //create new list item
     const newListItem = document.createElement('li');
+    //update the text of the new list item to be the input
     newListItem.textContent = thingToMake;
+    //add the new list item to the ul on the screen
     theList.appendChild(newListItem);
 }
